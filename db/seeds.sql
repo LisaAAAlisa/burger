cc4dc161e9761ea35454429faff63ef834dc432a
LisaAAAlisa <alisaamacedo@gmail.com>
2020-12-14 15:19:45 -0600

    initial setup, initial commit, pre-populating with params

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Cheeseburger', false);
+
+
+
