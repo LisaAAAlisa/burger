d3216f32c50a6ed5269ab2e4e99844f57a73a985
LisaAAAlisa <alisaamacedo@gmail.com>
2020-12-14 19:17:18 -0600

    adding JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "*batYA5kombat$",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  // connect to JawsDB (heroku)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // else connect to local database
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "*batYA5kombat$",
+    database: "burgers_db"
+  });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
